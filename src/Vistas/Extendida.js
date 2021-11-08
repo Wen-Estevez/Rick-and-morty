212029e05556c79b540d44b5fcc3b9e4284f6907
unknown <westevez04@gmail.com>
2021-11-08 12:43:53 -0500

    prueba tarjeta extendida movil

--- src/Vistas/Extendida.js
+++ src/Vistas/Extendida.js
@@ -1,5 +1,7 @@
 import { useParams } from "react-router";
 import { useState,useEffect } from "react";
+import './Extendida.css';
+import fondoExt from "../Images/fondoExt.jpg";
 
 export default function Extendida (){
     let id=useParams().id;
@@ -11,15 +13,20 @@ export default function Extendida (){
            setPersonaje(pj)
                  }
          })
-   useEffect(() => {fetchPersonaje()}, []);
+   useEffect(() => {fetchPersonaje()}, [id]);
     return(
+        <div>
+            <img className="fondoExt" src={`${fondoExt}`} />
         <div className="Extendida">
-            <img src={`${personaje.image}`}/><br/>
-            <span>Nombre: {`${personaje.name}`}</span><br/>
-            <span>Estado: {`${personaje.status}`}</span><br/>
-            <span>Especie: {`${personaje.species}`}</span><br/>
-            <span>Genero: {`${personaje.gender}`}</span><br/>
-            <span>Ubicación: {`${personaje.location.name}`}</span>
+            <img className="fotopersonaje" src={`${personaje.image}`}/><br/>
+            <div className="infopj">
+                <span>Nombre: {`${personaje.name}`}</span><br/>
+                <span>Estado: {`${personaje.status}`}</span><br/>
+                <span>Especie: {`${personaje.species}`}</span><br/>
+                <span>Genero: {`${personaje.gender}`}</span><br/>
+                <span>Ubicación: {`${personaje.location.name}`}</span>
+            </div>
+        </div>
         </div>
     )
 }
