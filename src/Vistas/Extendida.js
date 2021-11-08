df3bcca0f3eaf89ef6b76eb7a9d94064e93de757
unknown <westevez04@gmail.com>
2021-11-07 23:37:22 -0500

    tarjeta extendida

--- src/Vistas/Extendida.js
+++ src/Vistas/Extendida.js
@@ -1,12 +1,25 @@
-import { useParams } from "react-router"
+import { useParams } from "react-router";
+import { useState,useEffect } from "react";
 
-export default function Estendida (){
-   let tarjeta= useParams().id
-
+export default function Extendida (){
+    let id=useParams().id;
+   const [personaje,setPersonaje]=useState([]);
+   const fetchPersonaje= () => fetch(`https://rickandmortyapi.com/api/character/${id}`)
+     .then(response => response.json())
+     .then(pj => {
+         if (pj !== undefined){
+           setPersonaje(pj)
+                 }
+         })
+   useEffect(() => {fetchPersonaje()}, []);
     return(
-        <div>
-        soy una tarjeta extendida
-        {`${tarjeta}`}
+        <div className="Extendida">
+            <img src={`${personaje.image}`}/><br/>
+            <span>Nombre: {`${personaje.name}`}</span><br/>
+            <span>Estado: {`${personaje.status}`}</span><br/>
+            <span>Especie: {`${personaje.species}`}</span><br/>
+            <span>Genero: {`${personaje.gender}`}</span><br/>
+            <span>Ubicación: {`${personaje.location.name}`}</span>
         </div>
     )
 }
