620e235fbe80a34b2fa64bb8588a7cc594206cdc
unknown <westevez04@gmail.com>
2021-11-07 21:34:19 -0500

    version tablet

--- src/Componentes/TarjetasPersonajes.js
+++ src/Componentes/TarjetasPersonajes.js
@@ -5,9 +5,11 @@ export default function TarjetasPersonajes({name,img,status, species}){
     return(
         <div className="card" >
             <img className="imagen" src={`${img}`} alt="Imagen"/>
-            <span className="card-title">Nombre:  {name}  </span>
-            <span className="card-body">Estado: {status}</span>
-            <span className="card-body">Especie:  {species}</span>
+            <div className="info">
+                <span className="card-title">Nombre:  {name}  </span><br/>
+                <span className="card-body">Estado: {status}</span><br/>
+                <span className="card-body">Especie:  {species}</span>
+            </div>
         </div>
        
 
--- src/Vistas/Home.js
+++ src/Vistas/Home.js
@@ -3,6 +3,7 @@ import fondo from "../Images/fondo.jpg";
 import foto from "../Images/foto.jpg";
 import ContenedorPersonajes from '../Componentes/ContenedorPersonajes';
 import {useState} from 'react';
+import { VscChevronLeft,VscChevronRight } from "react-icons/vsc";
 
 
 export default function Home(){
@@ -12,16 +13,17 @@ export default function Home(){
         <div className="home">
             <img className="fondo" src={`${fondo}`}></img>
 
-            <div>
+            <div className="">
                 <img className="foto" src={`${foto}`}></img>       
                 <div className="botones">
-                    <button className="botones" onClick={(e) => {
-                        setPagina(pagina-1);
-                        }}> Anterior 
+                    <button className="btn" onClick={(e) => {
+                        if (pagina<=1){}
+                        else {setPagina(pagina-1)}
+                        }}> <VscChevronLeft size={30}/> 
                     </button>
-                    <button className="botones" onClick={(e) => {
+                    <button className="btn" onClick={(e) => {
                         setPagina(pagina+1);
-                        }}> Siguiente 
+                        }}> <VscChevronRight size={30}/>
                     </button>
             </div>     
         </div>
