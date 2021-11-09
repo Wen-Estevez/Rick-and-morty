7e5467f1a2be9ad9cf675309e744bfd22e16c02b
Wen-Estevez <westevez04@gmail.com>
2021-11-09 16:20:32 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import firebase from "./firebase-config"
+
 //Punto de acceso a la App
 
 ReactDOM.render(
