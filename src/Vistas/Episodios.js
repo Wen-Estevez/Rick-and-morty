e46235b8952e540f1f64e4ce865fe489146bc37a
Wen-Estevez <westevez04@gmail.com>
2021-11-08 21:02:04 -0500

    Contenedor de episodios y fetch

--- src/Vistas/Episodios.js
+++ src/Vistas/Episodios.js
@@ -1,10 +1,36 @@
-import { useParams } from "react-router";
+import { useState,useEffect } from "react";
+import { useParams} from "react-router";
+import TarjetasEpisodios from "../Componentes/TarjetasEpisodios"
+
 export default function Episodios (){
+    const [episodios,setEpisodios]=useState([]);
     let id=useParams().id;
-    console.log(id)
+    const fetchEps= ()=>fetch(`https://rickandmortyapi.com/api/character/${id}`)
+     .then(response => response.json())
+     .then(pj => pj.episode)
+     .then(episodios=>{
+        episodios.map((ep=>{
+            fetch(`${ep}`)
+                .then(response=>response.json())
+                .then(ep=>{
+                    setEpisodios(oldEpisodios => [...oldEpisodios, ep])
+                    })
+                    return ep;
+        }))
+     })
+
+     useEffect(() => {fetchEps()// eslint-disable-next-line react-hooks/exhaustive-deps
+    },[]);
+
     return(
         <div>
-            episodio
+            {episodios.map((ep)=><TarjetasEpisodios
+            key={ep.id}
+            id={ep.id}
+            name={ep.name}
+            date={ep.air_date}
+            episode={ep.episode}
+            />)}
         </div>
     )
 
