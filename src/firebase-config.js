d8a9fe6957a67d1771e5c68365aee87cdc097ad0
unknown <westevez04@gmail.com>
2021-11-09 13:53:29 -0500

    firebase

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firebase';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,5 +15,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export default app;
+const db = getFirestore(app);
+export default db;
