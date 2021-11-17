bfbdb1187237bdb753a8be27bda8aac86a5f5076
Wen-Estevez <westevez04@gmail.com>
2021-11-16 22:43:22 -0500

    documented

--- src/Componentes/TarjetasPersonajes.js
+++ src/Componentes/TarjetasPersonajes.js
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom';
 
 //Recibe la informacion y crea una tarjeta por cada personaje 
 
-export default function TarjetasPersonajes({name,img,status, species,id}){
+export default function TarjetasPersonajes({name,img,status, species,id}){      
 
+   
     return(
-        <Link className="card-body" to={`/${id}`}>
+        <Link className="card-body" to={`/${id}`}>                              {/* Crea un link, cuando da click en tarjeta envia la ruta /:id */}
         <div className="card" >
             <img className="imagen" src={`${img}`} alt="Imagen"/>
             <div className="info">
