54f398687dd8c55eb341c784fadae4ef9649c1cd
Wen-Estevez <westevez04@gmail.com>
2021-11-16 22:30:54 -0500

    documented

--- src/Vistas/Home.js
+++ src/Vistas/Home.js
@@ -8,18 +8,17 @@ import { VscChevronLeft,VscChevronRight } from "react-icons/vsc";
 //Pagina principal recibe el estado cuando cambie de paginas
 
 export default function Home(){
-    const [pagina,setPagina]=useState(1);
+    const [pagina,setPagina]=useState(1);           //Estado, guarda en que pagina esta
 
     return(
         <div className="home">
             <img className="fondo" src={`${fondo}`}alt="Fondo"></img>
-
             <div className="">
                 <img className="foto" src={`${foto}`} alt="Imagen personaje"></img>       
                 <div className="botones">
                     <button className="btn" onClick={(e) => {
                         if (pagina<=1){}
-                        else {setPagina(pagina-1)}
+                        else {setPagina(pagina-1)}      
                         }}> <VscChevronLeft size={30}/> 
                     </button>
                     <button className="btn" onClick={(e) => {
