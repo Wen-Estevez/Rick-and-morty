012c13c2eb99519ad2308060aa45614558c298a1
Wen-Estevez <westevez04@gmail.com>
2021-11-16 22:40:27 -0500

    Documented

--- src/Componentes/ContenedorPersonajes.js
+++ src/Componentes/ContenedorPersonajes.js
@@ -4,16 +4,16 @@ import './ContenedorPersonajes.css';
 
 //Contenedor de cartas de personajes, trae la informacion de las paginas
 
-export default function ContenedorPersonajes({pag}){
-    const [personajes,setPersonajes]=useState([]);
-    const fetchPersonajes= (pag) => fetch(`https://rickandmortyapi.com/api/character/?page=${pag}`)
-      .then(response => response.json())
-      .then(pagina => {
+export default function ContenedorPersonajes({pag}){            //Recibe el estado pagina
+    const [personajes,setPersonajes]=useState([]);              //Crea el estado personajes
+    const fetchPersonajes= (pag) => fetch(`https://rickandmortyapi.com/api/character/?page=${pag}`) //Promesa, trae informacion de la api
+      .then(response => response.json())                        //Si hay respuesta la convierte en json 
+      .then(pagina => {                                         //Guarda ese json en una variable, pagina
           if (pagina.results !== undefined){
-            setPersonajes(pagina.results)
+            setPersonajes(pagina.results)                       //Guarda la informacion recibida en el estado personajes
                   }
           })
-    useEffect(() => {fetchPersonajes(pag)}, [pag]);
+    useEffect(() => {fetchPersonajes(pag)}, [pag]);             //Cuando pagina se modifique, realiza la promesa
 
     return(
         <div className="contenedor">
