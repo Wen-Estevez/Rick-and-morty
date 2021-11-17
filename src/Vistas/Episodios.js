dd396e7b2e62fd7ac9ce72cda7241f7dcf7c6506
Wen-Estevez <westevez04@gmail.com>
2021-11-16 23:08:42 -0500

    documentado

--- src/Vistas/Episodios.js
+++ src/Vistas/Episodios.js
@@ -9,24 +9,24 @@ import {GoHome } from "react-icons/go";
 //Trae la informacion de cada episodio para crear tarjetas con la informacion
 
 export default function Episodios (){
-    const [episodios,setEpisodios]=useState([]);
-    let id=useParams().id;
-    const fetchEps= ()=>fetch(`https://rickandmortyapi.com/api/character/${id}`)
-     .then(response => response.json())
+    const [episodios,setEpisodios]=useState([]);                        //Crea el estado episodios
+    let id=useParams().id;                                              //Guarda la informacion de la barra de navegacion
+    const fetchEps= ()=>fetch(`https://rickandmortyapi.com/api/character/${id}`)    //Trae informacion del personaje
+     .then(response => response.json())                                  //Convierte la informacion en un json
      .then(pj => pj.episode)
      .then(episodios=>{
-        episodios.map((ep=>{
-            fetch(`${ep}`)
-                .then(response=>response.json())
+        episodios.map((ep=>{                    //Map para cada elemento del arreglo hace algo
+            fetch(`${ep}`)                                              //Para cada episodio va a hacer otra promesa
+                .then(response=>response.json())                        //guarda ese resultado en un json
                 .then(ep=>{
-                    setEpisodios(oldEpisodios => [...oldEpisodios, ep])
+                    setEpisodios(oldEpisodios => [...oldEpisodios, ep]) //Agrega cada uno de esos episodios al estado espisodios
                     })
                     return ep;
         }))
      })
 
      useEffect(() => {fetchEps()// eslint-disable-next-line react-hooks/exhaustive-deps
-    },[]);
+    },[]);                                                                   //cuando hay un cambio, llama al fetch
 
     return(
         <div>            
